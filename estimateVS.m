f3dc10041af77b73d2cb3fb3bcc8ddc0e3ce6bcf
Neuraldominator <52453661+Neuraldominator@users.noreply.github.com>
2021-10-13 22:20:27 +0200

    Create estimateVS.m

---
+++ estimateVS.m
@@ -0,0 +1,20 @@
+function [VSest,Kci] = estimateVS(CIin)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% computes the estimated VS from CI values assuming a von Mises (vM) distribution.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% input: CIin - correlation index value(s)
+%
+% output: VSest - vector strength 
+%         Kci   - concentration parameter of vM distribution
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+Ndata = length(CIin);
+Kci = zeros(Ndata, 1);  
+VSest = zeros(Ndata, 1);
+for k = 1:Ndata
+    cifun = @(K)(besseli(0,2*K) ./ besseli(0,K).^2 - CIin(k));
+    Kci(k) = fsolve(cifun, 1.0, optimset('Display','off'));
+    VSest(k) = besseli(0,2*Kvs(k)) ./ (besseli(0,Kci(k))^2);
+end
+
+end % [eof]
