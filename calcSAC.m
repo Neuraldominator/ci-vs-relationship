b4e6ff52efdcc0f577f260460783b4280c73bfcc
Neuraldominator <52453661+Neuraldominator@users.noreply.github.com>
2021-10-13 22:43:49 +0200

    Update calcSAC.m

--- calcSAC.m
+++ calcSAC.m
@@ -1,9 +1,9 @@
 function [SAC,SACtv,CI,CN,Nsp] = calcSAC(SPin,BW,T1,T2,TL)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Calculating shuffled autocorrelogram of spike trains (ver 0.9 by GA)
+% Calculating shuffled autocorrelogram of spike trains (ver 0.9)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Input 
-%  SPin: 1-D cell array of spike time vectors in ms 
+%  SPin: 1-D cell array of spike time vectors [ms] 
 %  BW: SAC time bin width [ms] 
 %  T1: starting time [ms] for the analysis 
 %  T2: ending time [ms] for the analysis 
@@ -15,11 +15,11 @@ function [SAC,SACtv,CI,CN,Nsp] = calcSAC(SPin,BW,T1,T2,TL)
 %   The values of resulting time vector will be between -TL and +TL.  
 %
 % Output: 
-%  SAC  : calculated SAC 
+%  SAC: calculated SAC values
 %  SACtv: time vector [ms] for SAC 
-%  CI   : correlation index = SAC at time zero 
-%  CN   : normalization factor = (#reps) * (#reps-1) * (rate^2) * (bin width) * (time length) 
-%  Nsp  : number of spikes 
+%  CI: correlation index (SAC at time zero) 
+%  CN: normalization factor = (#reps)*(#reps-1)*(rate^2)*(bin width)*(data length) 
+%  Nsp: number of spikes 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % preprocessing to remove spike times out of the analysis time window
@@ -32,17 +32,18 @@ end
 
 % SAC time/data vectors
 Nbins = ceil(TL/BW); 
-SACtv = (-Nbins:Nbins) * BW; % total number of bins = 2*Nbins + 1
-SAC = zeros(1,length(SACtv)); 
-Nsp = 0; % number of spikes
+SACtv = (-Nbins:Nbins) * BW;  % total number of bins = 2*Nbins + 1
+SAC = zeros(1, length(SACtv)); 
+Nsp = 0;  % number of spikes
 
 % main loop 
 for c = 1:length(SP)
   Nsp = Nsp + length(SP{c});
-  for k = [ 1:c-1, c+1:length(SP) ] % loop across trials
+  for k = [ 1:c-1, c+1:length(SP) ]  % loop across trials
     if( ~isempty(SP{c}) && ~isempty(SP{k}) )
       d = SP{c} - SP{k}'; % matrix containing spike time differences
-      v = d( d>-TL-BW/2 & d<TL+BW/2 ); % data within SAC time range
+      v = d( d>-TL-BW/2 & d<TL+BW/2 );  % data within SAC time range
+      
       % histogram counts
       for j = 1:length(v)
         i = round(v(j)/BW) + Nbins + 1;
@@ -53,9 +54,12 @@ for c = 1:length(SP)
 end
 
 % normalization
-T = T2-T1; % [ms] time length
-R = Nsp / (Nreps*T); % [spikes/ms] average spike rate
-CN = Nreps * (Nreps-1) * R^2 * BW * T; % normalization factor
-SAC = SAC/CN; % final results 
+R = Nsp / (Nreps*T);  % [spikes/ms] average spike rate
+T = T2-T1;  % [ms] data length
+CN = Nreps * (Nreps-1) * R^2 * BW * T;  % normalization factor (Louage norm)
+
+% final results 
+SAC = SAC/CN; 
 CI = SAC(Nbins+1); 
 
+end % [eof]
