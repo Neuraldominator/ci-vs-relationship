6f71c644841aac1fa3e350c94091c688a0224f86
Neuraldominator <52453661+Neuraldominator@users.noreply.github.com>
2021-10-13 22:47:43 +0200

    Update calcPhaseHist.m

--- calcPhaseHist.m
+++ calcPhaseHist.m
@@ -3,19 +3,19 @@ function [PH,PHtv,VS] = calcPhaseHist(SPin,T1,T2,NB,FQ)
 % Calculating phase histogram and vector strength shuffled of spike trains 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Input 
-%  SPin: 1-D cell array of spike time vectors in ms 
-%  T1: starting time [ms] for the analysis 
-%  T2: ending time [ms] for the analysis 
-%  NB : number of bins of the phase histogram
-%  FQ : frequency [Hz] 
+%   SPin: 1-D cell array of spike time vectors [ms] 
+%   T1: starting time [ms] for the analysis 
+%   T2: ending time [ms] for the analysis 
+%   NB: number of bins used for the phase histogram
+%   FQ: frequency [Hz] 
 %
 %  +++ Notes +++
 %   Spike times between T1 and T2 will be used for the analysis. 
 %
 % Output: 
-%  PH  : calculated period histogram
-%  PHtv: time vector for period histogram [cycle]
-%  VS  : vector strength
+%   PH: calculated period histogram values
+%   PHtv: time vector for period histogram [cycle]
+%   VS: vector strength
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % preprocessing to remove spike times out of the analysis time window
@@ -26,7 +26,7 @@ for c = 1:Nreps
   SP{c} = v( v>=T1 & v<=T2 );  
 end
 
-% VS 
+% vector strength
 VSnn = 0; 
 VScc = 0; 
 VSss = 0; 
@@ -41,9 +41,10 @@ VS = sqrt( VScc*VScc + VSss*VSss ) / VSnn;
 PHtv = (0:NB-1)/NB;
 PH = zeros(1,NB); 
 for c = 1:Nreps
- PHidx = floor( mod((FQ*SP{c}/1000),1) * NB) + 1; % phase index vector
+ PHidx = floor( mod((FQ*SP{c}/1000),1) * NB) + 1;  % phase index vector
  for i=1:length(PHidx)
    PH(PHidx(i)) = PH(PHidx(i)) + 1;
  end
 end
 
+end % [eof]
